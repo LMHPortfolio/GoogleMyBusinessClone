4c84c3c13b0f4687521ccc5ed913843acadd4738
Lauren Hansbury <your_github_email>
2025-02-24 20:01:27 -0500

    initial commit

---
+++ src/app/business-list/business-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-list',
+  imports: [],
+  templateUrl: './business-list.component.html',
+  styleUrl: './business-list.component.css'
+})
+export class BusinessListComponent {
+
+}

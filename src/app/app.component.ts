9a8fa64af0395a3bd0d1d26b5029a45e336c281b
Lauren Hansbury <your_github_email>
2025-03-04 22:16:07 -0500

    added header comp and side bar nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {BusinessListComponent} from './business-list/business-list.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {NavComponent} from './shared/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BusinessListComponent],
+  imports: [RouterOutlet, BusinessListComponent, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

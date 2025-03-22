f6cbae9afe5efeec533098dbaabd03f3c5be3416
Lauren Hansbury <your_github_email>
2025-03-22 16:12:13 -0400

    created business detail view and styles

--- src/app/business-detail/business-detail.component.ts
+++ src/app/business-detail/business-detail.component.ts
@@ -1,17 +1,29 @@
 import {Component, inject, Input, input} from '@angular/core';
 import {BusinessDataService} from '../business-data.service';
+import {HeaderComponent} from '../shared/header/header.component';
 
 @Component({
   selector: 'app-business-detail',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './business-detail.component.html',
   styleUrl: './business-detail.component.css'
 })
 export class BusinessDetailComponent {
 
+
   private businessService = inject(BusinessDataService);
 
-  businessName = this.businessService.businessData.name;
+  //will add more business info later for details that aren't shown on dashboard view
+
+  businessData = {
+
+    businessName: this.businessService.businessData.name,
+    businessLocation: this.businessService.businessData.location,
+    businessRating: this.businessService.businessData.rating,
+    businessTags: this.businessService.businessData.tags,
+
+  }
+  pageTitle = "Viewing: " + this.businessService.businessData.name;
 
 
 
